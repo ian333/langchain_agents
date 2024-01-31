bb7665282cb00fc845fcb428d9d941a34c8ed036
Ian <sebastianvaz123@hotmail.com>
2024-01-30 21:25:53 -0600

    se modifico el CORS y correccion de largo del id
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>
    
    Former-commit-id: 4abc5b65df81d68ce175ecb4f8ef3c2e562a4d4a
    Former-commit-id: e222c28031d2f7f3fe6c22392e8c450c4e83223b
    Former-commit-id: 4695214daf2dad5662fb82fae978e486777315f5

--- models/chat_models.py
+++ models/chat_models.py
@@ -1,7 +1,7 @@
 # chat_models.py
 from pydantic import BaseModel, Field
 import uuid
-from typing import Optional
+from typing import Optional,List
 
 class ChatRequest(BaseModel):
     text: str = Field(..., example="Hola, ¿cómo estás?")
@@ -9,5 +9,5 @@ class ChatRequest(BaseModel):
     memberid: Optional[str] = Field(None, example="d6d7b6b9-a530-4fa5-9375-fd6bc54b41f5")
     prompt: Optional[str] = Field(None, example="Explica el concepto de relatividad.")
     threadid: Optional[str] = Field(default_factory=lambda: str(uuid.uuid4()), example=str(uuid.uuid4()))
-    followup: Optional[str] = Field(None, example="Necesito más información sobre este tema.")
+    followup: Optional[List] = Field(None, example=[{"Answer":"¡Hola! Estoy bien, gracias. ¿Y tú? ¿En qué puedo ayudarte hoy?","Prompt": "Hola, ¿cómo estás?"}])
     email: Optional[str] = Field(None, example="sebastian@skills.tech")
