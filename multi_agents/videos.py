5b90ba46fa0bff7a8110bc453bf955c0a3e589a2
Ian <sebastianvaz123@hotmail.com>
2024-07-08 12:17:00 -0600

    Se modifico el orden del llamado y se añadio el true en websearch
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>
    
    Former-commit-id: ad503febe61e72f997d6fdd16bf6b2fd2d05d026
    Former-commit-id: dd3f8132e35a9010c048be31835594243916cc0e
    Former-commit-id: 1ae4f271dc81621aab68fc3216edc04a2d4f1d7f

--- multi_agents/videos.py
+++ multi_agents/videos.py
@@ -2,7 +2,8 @@ import os
 import re
 from decouple import config
 from supabase import create_client, Client
-import requests
+import httpx
+from database.supa import supabase_admin, supabase_user
 
 # Configuración de variables de entorno
 os.environ["OPENAI_API_KEY"] = config("OPENAI_API_KEY")
@@ -10,8 +11,6 @@ os.environ["ACTIVELOOP_TOKEN"] = config("ACTIVELOOP_TOKEN")
 os.environ["GOOGLE_API_KEY"] = config("GOOGLE_API_KEY")
 
 bucket_name = "CoursesFiles"
-import httpx
-from database.supa import supabase_admin,supabase_user
 
 class VideosQA:
     def __init__(self, courseid, id=None, first_response=None, thread_id=None):
@@ -23,12 +22,13 @@ class VideosQA:
     async def query(self, query_text):
         try:
             # Realizar la solicitud al nuevo servidor de bases de datos vectorizadas
-            api_url = "https://34.46.119.67/query"  # Añadir http://127.0.0.1:8000/
-            api_url = "http://127.0.0.1:8000/query"  # Añadir http://127.0.0.1:8000/
+            api_url = "https://34.46.119.67/query"
+            api_url = "http://127.0.0.1:8000/query"
+
             payload = {
                 "courseid": self.courseid,
                 "query_text": query_text,
-                "type": "VIDEO"  # O "PDF" según corresponda
+                "type": "VIDEO"
             }
             print(f"Sending request to {api_url} with payload: {payload}")
             async with httpx.AsyncClient(verify=False) as client:
@@ -38,7 +38,7 @@ class VideosQA:
             print(f"Received response: {result}")
 
             videos = []
-            for i, doc in enumerate(result.get("source_documents", []), start=1):
+            for i, doc in enumerate(result, start=1):
                 video_id_match = re.search(r"v=([a-zA-Z0-9_-]+)", doc['metadata'].get('source', ''))
                 url = doc['metadata'].get('source', '')
                 if video_id_match:
@@ -55,17 +55,7 @@ class VideosQA:
 
             data = {"videos": videos}
             return data
-            print(f"Videos data to be updated in Supabase for course ID {self.courseid}: {data}")
 
-            try:
-
-                supabase_user.table("threads_tb").update({"thread_img": videos[0]}).eq("id", self.thread_id).execute()
-                supabase_user.table("responses_tb").update({"videos": data}).eq("id", self.id).execute()
-                print(f"Supabase updated with videos for course ID {self.courseid}")
-            except Exception as e:
-                print(f"Error al actualizar la base de datos: {e}")
-
-            return result if videos else {"error": "No se encontraron documentos."}
         except Exception as e:
             print(f"Error during query: {e}")
             return {"error": "No se pudo conectar a la base de datos esta vacia"}
