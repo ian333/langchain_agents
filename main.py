135c212e3e721c51322f2d33ff416a6c31ba4149
Ian <sebastianvaz123@hotmail.com>
2024-07-12 00:35:33 -0600

    Se modifico el orden del llamado y se añadio el true en websearch
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>
    
    Former-commit-id: 69c1324a8c9c79616296aba0381e15ca58b540ad
    Former-commit-id: 925f59a52af6c586f3a160f9ecf700d546985ac4
    Former-commit-id: 8eb622d54a7f1c62da81162a5da5f5a05d6b8fd1

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 # main.py
-from uuid import uuid4  # Importa esta biblioteca en la parte superior de tu archivo
-
-# Importaciones de librerías estándar
+from uuid import uuid4
 from datetime import datetime
 import time
 import asyncio
 
-# Importaciones de FastAPI
-from fastapi import FastAPI, HTTPException, Request, Security, BackgroundTasks, Header
-from fastapi import status
-
-# Importaciones de Langchain y herramientas relacionadas
+from fastapi import FastAPI, HTTPException, Request, Security, BackgroundTasks, Header, status
 from langchain.agents import initialize_agent, AgentType
 from langchain.callbacks.streaming_stdout_final_only import FinalStreamingStdOutCallbackHandler
 
-# Importaciones de utilidades y modelos propios
 from utils.db_utils import save_reminder
 from models.chat_models import ChatRequest, QueryRequest
 from examples.chat_examples import chat_examples
 from multi_agents.agent_executor import run_agent
-from multi_agents.agent_utils import process_course_info  
+from multi_agents.agent_utils import process_course_info
 from starlette.middleware.cors import CORSMiddleware
 
-# Importaciones de configuración y utilidades adicionales
 from decouple import config
 from supabase import create_client
 
-# Proyecto Admin
-import os
 from langchain.globals import set_debug
 set_debug(False)
 
@@ -36,34 +26,32 @@ from multi_agents.sources import SourcesQA
 from multi_agents.web_search import WebSearch
 from multi_agents.follow_up import run_follow
 from database.supa import supabase_user
+import os
 
 os.environ["OPENAI_API_KEY"] = config("OPENAI_API_KEY")
 
-url_admin: str = config("SUPABASE_ADMIN_URL")
-key_admin: str = config("SUPABASE_ADMIN_KEY")
+url_admin = config("SUPABASE_ADMIN_URL")
+key_admin = config("SUPABASE_ADMIN_KEY")
 
 supabase_admin = create_client(supabase_url=url_admin, supabase_key=key_admin)
 
 from Config.config import set_language, get_language
 
-# Configuración de la aplicación FastAPI
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permite todas las origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Permite todos los métodos
-    allow_headers=["*"],  # Permite todos los headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 HEADER_NAME = "X-API-KEY"
 api_key_header = Header(HEADER_NAME)
 
 async def validate_api_key(api_key_header: str = Security(api_key_header)):
-    API_KEY: str = config("API_KEY")
-    print(API_KEY)
-    print(api_key_header)
+    API_KEY = config("API_KEY")
     if api_key_header != API_KEY:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -73,29 +61,16 @@ async def validate_api_key(api_key_header: str = Security(api_key_header)):
 
 @app.post("/chat", status_code=status.HTTP_200_OK)
 async def chat_endpoint(request_body: ChatRequest, background_tasks: BackgroundTasks, api_key: str = validate_api_key):
-    """
-    Endpoint para procesar solicitudes de chat, interactuar con un agente y guardar la información en la base de datos.
-
-    Args:
-    - request_body (ChatRequest): Datos de la solicitud de chat.
-
-    Returns:
-    - dict: Respuesta del agente y otros detalles relevantes.
-    """
     start_time = time.time()
 
-    # Desempaquetar los datos recibidos
     courseid = request_body.courseid
     memberid = request_body.memberid
     prompt = request_body.prompt
-    threadid = request_body.threadid  # Asume que ya viene generado si es necesario
+    threadid = request_body.threadid
     followup = request_body.followup
     email = request_body.email
     orgid = request_body.organizationid
     web = request_body.web
-
-    if not threadid:
-        threadid = str(uuid4())
 
     # Obtener instrucciones de la empresa desde la tabla de admin
     response = supabase_admin.table("courses_tb").select("*").eq("id", courseid).execute()
@@ -118,7 +93,7 @@ async def chat_endpoint(request_body: ChatRequest, background_tasks: BackgroundT
         set_language("english")
         print(f"\033[92mSe definió el lenguaje 😁😁 como {get_language()}\033[0m")
 
-    user_data = {   
+    user_data = {
         "courseid": courseid,
         "memberid": memberid,
         "prompt": prompt,
@@ -138,7 +113,6 @@ async def chat_endpoint(request_body: ChatRequest, background_tasks: BackgroundT
         sources = SourcesQA(courseid=courseid, orgid=orgid)
         follow_task = run_follow(query=prompt, history=followup)
         
-        # Ejecuta las tareas en paralelo
         video_task = videos.query(prompt)
         source_task = sources.query(prompt)
         video, source, follow_up_questions = await asyncio.gather(video_task, source_task, follow_task)
@@ -160,10 +134,44 @@ async def chat_endpoint(request_body: ChatRequest, background_tasks: BackgroundT
 
     return {"thread_id": threadid}
 
+@app.post("/create-thread", status_code=status.HTTP_200_OK)
+async def create_thread(request_body: ChatRequest, background_tasks: BackgroundTasks, api_key: str = validate_api_key):
+    """
+    Endpoint para crear un nuevo thread y luego llamar al chat_endpoint para continuar con el proceso de chat.
+
+    Args:
+    - request_body (ChatRequest): Datos de la solicitud de chat.
+
+    Returns:
+    - dict: Respuesta del agente y otros detalles relevantes.
+    """
+    # Crear un nuevo thread ID
+    new_thread_id = str(uuid4())
+
+    # Guardar el nuevo thread en la base de datos
+    thread_data = {
+        "id": new_thread_id,
+        "threadname": request_body.prompt,
+        "memberid": request_body.memberid,
+        "courseid": request_body.courseid,
+        "created_at": datetime.now().isoformat(),
+        "organizationid": request_body.organizationid,
+        "first_response": None
+    }
+    supabase_user.table("threads_tb").insert(thread_data).execute()
+
+    # Actualizar el request_body con el nuevo thread ID
+    request_body.threadid = new_thread_id
+
+    # Llamar al chat_endpoint para continuar el proceso de chat
+    background_tasks.add_task(chat_endpoint, request_body, background_tasks, api_key)
+
+    return {"thread_id": new_thread_id}
+
 from database.Vector_database import VectorDatabaseManager
-print("Se estan inicializando las VEctorDatabase")
+print("Se estan inicializando las VectorDatabase")
 vector_db_manager = VectorDatabaseManager()
-print("Se estan inicializando las VEctorDatabase")
+print("Se estan inicializando las VectorDatabase")
 
 @app.post("/query")
 async def query_database(request: QueryRequest):
@@ -175,4 +183,3 @@ async def query_database(request: QueryRequest):
     except Exception as e:
         print(f"\033[91mError processing query: {e}\033[0m")
         raise HTTPException(status_code=500, detail=str(e))
-
